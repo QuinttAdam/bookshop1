c66153e5cc5aa83303ba510bf2b39d7849bf53e1
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2022-11-01 15:30:42 +0100

    commit5

--- App.js
+++ App.js
@@ -9,22 +9,32 @@ export default function App() {
       <View style={styles.head}>
       <Text style={styles.titel}>Bookshop</Text>
 
-      <View>
+      <View style={styles.mand}>
         <Image style={styles.icon}
               source={require('./assets/winkelkar.png')}
             />
-        <Text>1</Text>
+        <Text style={styles.aantal}>1</Text>
       </View>
       </View>
       
-      
+      <View>
+        <Text>Book title <Text>1</Text></Text>
+        <Text>By Some Random dude</Text>
+        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+        </Text>
+        <View style={styles.code}>
+          <Text>ISBN 978-8-2665-1860-1</Text>
+          <Text>READ</Text>
+        </View>
+        
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-
+    margin: 20,
   },
   titel: {
     
@@ -33,7 +43,7 @@ const styles = StyleSheet.create({
   },
   head: {
     marginTop: 40,
-    margin: 20,
+    
     display: 'flex',
     flexDirection: "row",
     justifyContent: 'space-between'
@@ -43,5 +53,19 @@ const styles = StyleSheet.create({
   icon: {
     width: 40,
     maxHeight: 40,
+  },
+  mand:{
+    display: 'flex',
+    flexDirection: "row",
+  },
+  aantal:{
+    fontSize: 20,
+    marginTop: 25,
+  },
+  code:{
+    marginTop: 20,
+    display: 'flex',
+    flexDirection: "row",
+    justifyContent:"space-between",
   }
 });
